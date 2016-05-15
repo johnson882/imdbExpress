ab8d0645f5967a5eb28e77ccf0608bef3d506393
Ian Johnson <johnson882@gmail.com>
2016-05-15 00:48:26 -0700

    Second Commit

--- expresstut.js
+++ expresstut.js
@@ -25,11 +25,23 @@ app.get('/contact', function(req, res){
 })
 
 app.get('/about', function(req, res){
-  res.render('about');
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', {fortune: randomFortune});
 });
 
+//404 catch-all handler(middleware)
 app.use(function(req, res, next){
-//m
+res.status(404);
+res.render('404');
+});
+
+
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+
 });
 
 app.set('port', process.env.PORT || 3000);
@@ -39,6 +51,12 @@ app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function(){
   console.log("Express started on http://localhost:" + app.get('port') + ' press Ctrl-c to terminate');
 });
+
+var fortunes = ["You will win money in your next gambling experience", 
+				"you will make a great fortune from selling online hair ties",
+				"never give up and you will do great!",
+				"you will get a great surprise tomorrow at 2:00PM",
+				"what ever you do keep things simple!"];
 /*
 app.get('/', function(req,res){
   res.send('Express Works');
